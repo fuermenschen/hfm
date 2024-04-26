f82afd008719911de9348605af41012d6ead473f
moss <moss@zhaw.ch>
2024-04-26 18:47:05 +0200

    Update system configuration and dependencies, and add new mail class
    This commit includes several updates. The main changes are updating the system configuration files and upgrading the project's dependencies to the latest versions. In addition to this, the previous exclusion folders were removed and new ones were added to the IDE. We also added a new class for sending mail when an athlete is registered.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <x-button icon="information-circle" label="Erfahre mehr" href="/ueber-das-projekt" wire:navigate red xl/>
         <x-button icon="fire" label="Jetzt Sportler:in werden" href="/sportlerin-werden" wire:navigate
                   red xl/>
-        <x-button icon="heart" label="Jetzt Spender:in werden" href="/spenderin-werden" wire:navigate
+        <x-button icon="heart" label="Jetzt Sponsor:in werden" href="/sponsorin-werden" wire:navigate
                   red xl/>
         <x-button icon="flag" label="Details zum Anlass" href="/informationen-zum-anlass" wire:navigate red xl/>
     </div>
