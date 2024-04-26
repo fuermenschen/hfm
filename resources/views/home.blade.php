8cb81c902e7240beebac743cf4d0d0ebca7b402d
smnmsr <simon.moser@mailbox.org>
2024-04-26 12:22:20 +0200

    Update layout styling in home view
    The layout styling in the home view has been updated. The grid div's class has been changed to include additional classes for better responsiveness and design optimization on smaller screens.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
     <x-logo-and-text class="mx-auto w-[600px] max-w-full p-10"/>
     Es haben sich schon {{ $athleteCount }} Sportler:innen registriert. Mache auch du mit bei der wohltätigen Aktion!
 
-    <div class="sm:grid sm:grid-cols-2 max-w-full sm:gap-md mt-md">
+    <div class="flex flex-col space-y-sm sm:grid sm:grid-cols-2 max-w-full sm:space-y-0 sm:gap-md mt-md">
         <x-button icon="information-circle" label="Erfahre mehr" href="/ueber-das-projekt" wire:navigate red xl/>
         <x-button icon="fire" label="Jetzt Sportler:in werden" href="/sportlerin-werden" wire:navigate
                   red xl/>
