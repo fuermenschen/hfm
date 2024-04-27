6cc01836272e1439aa9458d31846684e585aa1fe
PascalStrebel <33620588+PascalStrebel@users.noreply.github.com>
2024-04-27 11:50:55 +0200

    Update become-athlete.blade.php

--- resources/views/pages/become-athlete.blade.php
+++ resources/views/pages/become-athlete.blade.php
@@ -2,14 +2,12 @@
     @component('components.page-title')
         Sportler:in werden
     @endcomponent
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias amet animi aperiam aspernatur atque autem,
-    beatae commodi consequatur corporis cumque delectus doloremque doloribus ducimus ea eius eligendi eos error est eum
-    ex explicabo facere fugiat fugit harum id illum impedit in incidunt ipsa ipsam ipsum iure laborum laudantium magnam
-    magni maiores maxime minima minus molestias mollitia natus nemo nesciunt nihil nisi nobis non nostrum numquam
-    obcaecati odio officiis omnis optio pariatur perferendis perspiciatis placeat possimus praesentium provident quae
-    quam quas qui quia quibusdam quisquam quo ratione recusandae rem repellendus repudiandae rerum saepe sapiente sequi
-    similique sit soluta sunt suscipit tempora tenetur totam ullam unde vel veniam veritatis voluptas voluptates
-    voluptatum.
+Willkommen bei "Höhenmeter für Menschen"! Wir laden dich herzlich ein, Teil dieses besonderen Sponsorenlaufs zu sein, bei dem deine Leidenschaft für Sport mit einem guten Zweck verbunden wird.
+Du legst die Strecke im Brühlbergquartier so oft zurück wie möglich und sammelst dabei für jede Runde Spendengelder deiner Sponsoren für den Benefizpartner deiner Wahl. Du kannst selbst bestimmen, wie du die Strecke zurücklegst, wichtig ist nur, dass du die Strecke mit eigener Kraft zurücklegst. Mögliche Optionen sind Joggen, Gehen, Fahrradfahren, Skaten oder die Strecke mit dem Rollstuhl zurücklegen.
+Alle sind willkommen: Egal ob Profi oder Gelegenheitssportler, Jung oder Alt, Laufend, gehend, auf dem Fahrrad oder im Rollstuhl - bei "Höhenmeter für Menschen" ist jeder willkommen, der sich für unsere Benefizpartner engagieren möchte.
+Strecke und Spenden: Die Strecke umfasst 1.7 km und 50 Höhenmeter. Für jede Runde, die du absolvierst, sammelt du Spenden von euren großzügigen Sponsoren. Jede Runde zählt und macht einen Unterschied!
+Verpflegung am Start: Damit du bestens gestärkt in den Event starten kannst, bieten wir euch Verpflegung am Startpunkt an. Tankt Energie und genießt die Gemeinschaft!
+Fragen? Bei Fragen stehen wir gerne unter <a href="mailto:members@rt25.ch">members@rt25.ch</a> zur Verfügung.
 
     <x-page-subtitle>
         Anmeldeformular
