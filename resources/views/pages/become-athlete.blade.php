87ae75c084b217c4b225b64f942da6f853e5b24b
PascalStrebel <33620588+PascalStrebel@users.noreply.github.com>
2024-04-27 11:59:27 +0200

    Update become-athlete.blade.php

--- resources/views/pages/become-athlete.blade.php
+++ resources/views/pages/become-athlete.blade.php
@@ -2,7 +2,7 @@
     @component('components.page-title')
         Sportler:in werden
     @endcomponent
-Willkommen bei "Höhenmeter für Menschen"! Wir laden dich herzlich ein, Teil dieses besonderen Sponsorenlaufs zu sein, bei dem deine Leidenschaft für Sport mit einem guten Zweck verbunden wird.
+Dein Einsatz für ein Lächeln – Lauf, fahre, rolle für Hilfe, die zu 100% ankommt! Wir laden dich herzlich ein, Teil dieses besonderen Sponsorenlaufs zu sein, bei dem deine Leidenschaft für Sport mit einem guten Zweck verbunden wird.
 Du legst die Strecke im Brühlbergquartier so oft zurück wie möglich und sammelst dabei für jede Runde Spendengelder deiner Sponsoren für den Benefizpartner deiner Wahl. Du kannst selbst bestimmen, wie du die Strecke zurücklegst, wichtig ist nur, dass du die Strecke mit eigener Kraft zurücklegst. Mögliche Optionen sind Joggen, Gehen, Fahrradfahren, Skaten oder die Strecke mit dem Rollstuhl zurücklegen.
 Alle sind willkommen: Egal ob Profi oder Gelegenheitssportler, Jung oder Alt, Laufend, gehend, auf dem Fahrrad oder im Rollstuhl - bei "Höhenmeter für Menschen" ist jeder willkommen, der sich für unsere Benefizpartner engagieren möchte.
 Strecke und Spenden: Die Strecke umfasst 1.7 km und 50 Höhenmeter. Für jede Runde, die du absolvierst, sammelt du Spenden von euren großzügigen Sponsoren. Jede Runde zählt und macht einen Unterschied!
