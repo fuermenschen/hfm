2da4992a3a4ceae34f77cd19e1dedbc05e006869
PascalStrebel <33620588+PascalStrebel@users.noreply.github.com>
2024-04-27 12:17:01 +0200

    Update event-information.blade.php

--- resources/views/pages/event-information.blade.php
+++ resources/views/pages/event-information.blade.php
@@ -1,4 +1,41 @@
 @component('components.page-title')
     Informationen zum Anlass
 @endcomponent
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci alias amet animi aperiam aspernatur atque autem, beatae commodi consequatur corporis cumque delectus doloremque doloribus ducimus ea eius eligendi eos error est eum ex explicabo facere fugiat fugit harum id illum impedit in incidunt ipsa ipsam ipsum iure laborum laudantium magnam magni maiores maxime minima minus molestias mollitia natus nemo nesciunt nihil nisi nobis non nostrum numquam obcaecati odio officiis omnis optio pariatur perferendis perspiciatis placeat possimus praesentium provident quae quam quas qui quia quibusdam quisquam quo ratione recusandae rem repellendus repudiandae rerum saepe sapiente sequi similique sit soluta sunt suscipit tempora tenetur totam ullam unde vel veniam veritatis voluptas voluptates voluptatum.
+
+
+    <x-page-subtitle>
+        Location
+    </x-page-subtitle>
+
+Brühlgut Stiftung, Brühlbergstrasse 6, 8400 Winterthur
+
+    <x-page-subtitle>
+        Ablauf
+    </x-page-subtitle>
+
+Der Sponsorenlauf findet am Samstag, den 21. September 2024, in Winterthur statt. Der Anlass dauert von 13:00 - 18:00. Während dieser Zeit können die Teilnehmer den Rundkurs so oft wie möglich zurücklegen. Der Rundkurs liegt im Brühlberg Quartier und die Brühlgut Stiftung dient als Start/Ziel und Informationsstand. 
+
+
+    <x-page-subtitle>
+        Anreise
+    </x-page-subtitle>
+
+Wir empfehlen die Anreise per öffentlichem Verkehr. Vom Bahnhof Winterthur fährt alle paar Minuten ein Bus zur Haltestellen <a href="https://maps.app.goo.gl/d6ef7GeKnWW1EwF49">Loki</a> 
+
+    <x-page-subtitle>
+        Sanitäre Anlagen
+    </x-page-subtitle>
+
+Toiletten befinden sich im Inneren des Gebäudes der Brühlgut Stiftung. Garderoben und Duschen stehen nicht zur Verfügung.
+
+    <x-page-subtitle>
+        Verpflegung
+    </x-page-subtitle>
+
+Es wird kleinere Snacks wie Sanwiches und Nussgipfel vor Ort geben, da der Event nicht über Mittag stattfindet, sind diese allerdings keine vollwertige Mahlzeit.
+
+    <x-page-subtitle>
+        Versicherung
+    </x-page-subtitle>
+
+Versicherung ist Sache der Teilnehmenden. Es wird keine Haftung für Unfälle aller Art oder verlorene und beschädigte Gegenstände übernommen.
