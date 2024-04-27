d0887e8a6c6276e782b3b4aa29bd617efa5b3028
moss <simon.moser@mailbox.org>
2024-04-27 21:22:34 +0200

    Update button label in home view
    Updated the label text in a button on the home page view. The button previously labelled as "Jetzt Sponsor:in werden" has been altered to "Jetzt Spender:in werden" effectively changing the translation from "Become a sponsor" to "Become a donor".

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.public')
 
 @section('content')
-<div>
-    @component('components.page-title')
-        Höhenmeter für Menschen
-    @endcomponent
-    Es haben sich schon {{ $athleteCount }} Sportler:innen registriert. Mache auch du mit bei der wohltätigen Aktion!
+    <div>
+        @component('components.page-title')
+            Höhenmeter für Menschen
+        @endcomponent
+        Es haben sich schon {{ $athleteCount }} Sportler:innen registriert. Mache auch du mit bei der wohltätigen
+        Aktion!
 
-    <div class="flex flex-col space-y-sm sm:grid sm:grid-cols-2 max-w-full sm:space-y-0 sm:gap-md mt-md">
-        <x-button icon="information-circle" label="Erfahre mehr" href="/ueber-das-projekt" wire:navigate red xl/>
-        <x-button icon="fire" label="Jetzt Sportler:in werden" href="/sportlerin-werden" wire:navigate
-                  red xl/>
-        <x-button icon="heart" label="Jetzt Sponsor:in werden" href="/sponsorin-werden" wire:navigate
-                  red xl/>
-        <x-button icon="flag" label="Details zum Anlass" href="/informationen-zum-anlass" wire:navigate red xl/>
+        <div class="flex flex-col space-y-sm sm:grid sm:grid-cols-2 max-w-full sm:space-y-0 sm:gap-md mt-md">
+            <x-button icon="information-circle" label="Erfahre mehr" href="/ueber-das-projekt" wire:navigate red xl />
+            <x-button icon="fire" label="Jetzt Sportler:in werden" href="/sportlerin-werden" wire:navigate
+                      red xl />
+            <x-button icon="heart" label="Jetzt Spender:in werden" href="/spenderin-werden" wire:navigate
+                      red xl />
+            <x-button icon="flag" label="Details zum Anlass" href="/informationen-zum-anlass" wire:navigate red xl />
+        </div>
     </div>
-</div>
 
 @endsection
