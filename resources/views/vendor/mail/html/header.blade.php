4810792747cb7ed47de8dbbed41b312984fe1311
smnmsr <simon.moser@mailbox.org>
2024-06-01 20:21:58 +0200

    bugfix email logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,9 @@
 @props(['url'])
+@php use Illuminate\Support\Facades\Vite; @endphp
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;" class="logo">
-<img src="{{ \Illuminate\Support\Facades\Vite::asset("resources/images/logo_dark.svg") }}}" style="width: 100%; height: auto;" alt="logo">
+<img src="{{ Vite::asset("resources/images/logo_dark.svg") }}" style="width: 100%; height: auto;" alt="logo">
 </a>
 </td>
 </tr>
