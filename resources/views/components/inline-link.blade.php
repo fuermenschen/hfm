cc0f2ed0230e52e5f7876a30ef477bf1510b196f
moss <simon.moser@mailbox.org>
2024-06-02 10:24:09 +0200

    bugfix: targeted links had wire:navigate inside

--- resources/views/components/inline-link.blade.php
+++ resources/views/components/inline-link.blade.php
@@ -1,3 +1,10 @@
-<a wire:navigate.hover {{ $attributes->merge(["class" => "text-hfm-red dark:text-hfm-lightred underline hover:text-hfm-light"]) }}>
+<a
+    {{ $attributes->merge(["class" => "text-hfm-red dark:text-hfm-lightred underline hover:text-hfm-light"]) }}
+    @if ($attributes->has(['target']))
+        target="{{ $attributes->get('target') }}"
+    @else
+        wire:navigate.hover
+    @endif
+>
     {{ $slot }}
 </a>
