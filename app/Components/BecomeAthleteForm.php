c29958145ca87021b8af1547649a23361f2aabd0
Simon Moser <simon.github@mailbox.org>
2025-04-24 17:40:26 +0200

    Refactor become-athlete form to use Flux components.
    Replaced Blade UI components with Flux components for consistency and improved styling. Updated phone number validation to use a regex pattern and changed default value of the privacy field to true.

--- app/Components/BecomeAthleteForm.php
+++ app/Components/BecomeAthleteForm.php
@@ -65,7 +65,7 @@ class BecomeAthleteForm extends Component
     // Telefonnummer
     #[Validate("required", message: "Wir benötigen deine Telefonnummer.")]
     #[Validate("string", message: "Wir benötigen deine Telefonnummer.")]
-    #[Validate("size:10", message: "Die Telefonnummer besteht aus 10 Zahlen.")]
+    #[Validate("regex:/^0\d{2}(?:\d{7}|\s\d{3}\s\d{2}\s\d{2})$/", message: "Die Telefonnummer ist ungültig.")]
     public ?string $phone_number = null;
 
     // Volljährig?
