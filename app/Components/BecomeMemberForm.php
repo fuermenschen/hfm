91baf07ecac52f76041d434425d4e8a3b031e09c
Simon <simon.github@mailbox.org>
2025-04-27 09:40:42 +0200

    Enhance member registration form with improved validation and layout

--- app/Components/BecomeMemberForm.php
+++ app/Components/BecomeMemberForm.php
@@ -2,11 +2,63 @@
 
 namespace App\Components;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class BecomeMemberForm extends Component
 {
-    public string $test = '';
+    // Name der Firma (optional)
+    #[Validate('string', message: 'Der Name der Firma muss ein Text sein.')]
+    #[Validate('max:255', message: 'Der Name der Firma darf nicht länger als 255 Zeichen sein.')]
+    public ?string $company_name = null;
+
+    // Vorname
+    #[Validate('required', message: 'Wir benötigen deinen Vornamen.')]
+    #[Validate('string', message: 'Der Vorname muss ein Text sein.')]
+    #[Validate('max:255', message: 'Der Vorname darf nicht länger als 255 Zeichen sein.')]
+    public ?string $first_name = null;
+
+    // Nachname
+    #[Validate('required', message: 'Wir benötigen deinen Nachnamen.')]
+    #[Validate('string', message: 'Der Nachname muss ein Text sein.')]
+    #[Validate('max:255', message: 'Der Nachname darf nicht länger als 255 Zeichen sein.')]
+    public ?string $last_name = null;
+
+    // Adresse
+    #[Validate('required', message: 'Wir benötigen deine Adresse.')]
+    #[Validate('string', message: 'Die Adresse muss ein Text sein.')]
+    #[Validate('max:255', message: 'Die Adresse darf nicht länger als 255 Zeichen sein.')]
+    public ?string $address = null;
+
+    // PLZ
+    #[Validate('required', message: 'Wir benötigen deine Postleitzahl.')]
+    #[Validate('integer', message: 'Die Postleitzahl muss eine Zahl sein.')]
+    #[Validate('digits:4', message: 'Die Postleitzahl muss vier Ziffern haben.')]
+    public ?int $zip_code = null;
+
+    // Ort
+    #[Validate('required', message: 'Wir benötigen deinen Wohnort.')]
+    #[Validate('string', message: 'Der Wohnort muss ein Text sein.')]
+    #[Validate('max:255', message: 'Der Wohnort darf nicht länger als 255 Zeichen sein.')]
+    public ?string $city = null;
+
+    // E-Mail
+    #[Validate('required', message: 'Wir benötigen deine E-Mail-Adresse.')]
+    #[Validate('email', message: 'Bitte gib eine gültige E-Mail-Adresse ein.')]
+    #[Validate('unique:members,email', message: 'Die E-Mail-Adresse ist bereits registriert.')]
+    public ?string $email = null;
+
+    // E-Mail bestätigen
+    #[Validate('required', message: 'Wir benötigen die Bestätigung deiner E-Mail-Adresse.')]
+    #[Validate('same:email', message: 'Die E-Mail-Adressen stimmen nicht überein.')]
+    public ?string $email_confirmation = null;
+
+    // Telefonnummer
+
+    #[Validate('required', message: 'Wir benötigen deine Telefonnummer.')]
+    #[Validate('string', message: 'Wir benötigen deine Telefonnummer.')]
+    #[Validate('digits:10', message: 'Die Telefonnummer ist ungültig.')]
+    public ?string $phone_number = null;
 
     public function render()
     {
@@ -15,6 +67,8 @@ class BecomeMemberForm extends Component
 
     public function submit()
     {
-        $this->test = 'Submitting works!';
+        $this->validate();
+
+        $this->reset();
     }
 }
--- resources/views/forms/become-member-form.blade.php
+++ resources/views/forms/become-member-form.blade.php
@@ -1,9 +1,108 @@
 <form wire:submit="submit"
-      class="flex flex-col space-y-sm sm:grid-cols-2 sm:grid max-w-full sm:space-y-0 sm:gap-sm mt-sm">
+      class="flex flex-col space-y-sm sm:grid sm:grid-cols-2 max-w-full sm:space-y-0 sm:gap-sm mt-sm">
 
-    <flux:input label="Test" name="test" wire:model.live="test" placeholder="gib etwas ein" required
-                class="sm:col-span-2" />
-    <flux:heading>{{ $this->test }}</flux:heading>
+    <flux:input
+        icon-trailing="user"
+        label="Vorname"
+        wire:model.blur="first_name"
+        placeholder="Francesca"
+        required
+        autocomplete="given-name"
+    />
+
+    <flux:input
+        icon-trailing="user"
+        label="Nachname"
+        wire:model.blur="last_name"
+        placeholder="Arslan"
+        required
+        autocomplete="family-name"
+    />
+
+    <flux:input
+        icon-trailing="building-office"
+        label="Firma"
+        wire:model.blur="company_name"
+        placeholder="Zukunft GmbH"
+        badge="optional"
+        autocomplete="organization"
+    />
+
+    <span class="hidden sm:block">
+        <!-- Placeholder for layout purposes -->
+    </span>
+
+    <flux:input
+        icon-trailing="home"
+        label="Adresse"
+        placeholder="Zelglistrasse 41"
+        wire:model.blur="address"
+        required
+        autocomplete="street-address"
+    />
+
+    <span class="flex flex-row space-x-4">
+            <span class="basis-1/3">
+                <flux:input
+                    icon-trailing="home"
+                    label="PLZ"
+                    placeholder="8406"
+                    wire:model.blur="zip_code"
+                    class="basis-1/3"
+                    required
+                    autocomplete="postal-code"
+                    mask="9999"
+                />
+            </span>
+            <span class="grow">
+                <flux:input
+                    icon-trailing="home"
+                    label="Ort"
+                    placeholder="Winterthur"
+                    wire:model.blur="city"
+                    class="grow"
+                    required
+                    autocomplete="address-level2"
+                />
+            </span>
+        </span>
+
+    <flux:input
+        icon-trailing="envelope"
+        label="E-Mail"
+        placeholder="francesca.arslan@posteo.ch"
+        wire:model.blur="email"
+        required
+        type="email"
+        autocomplete="email"
+    />
+
+    <flux:input
+        icon-trailing="envelope"
+        label="E-Mail bestätigen"
+        placeholder="francesca.arslan@posteo.ch"
+        wire:model.blur="email_confirmation"
+        required
+        type="email"
+        autocomplete="off"
+    />
+
+    <flux:input
+        icon-trailing="phone"
+        label="Telefon"
+        placeholder="079 123 45 67"
+        wire:model.blur="phone_number"
+        required
+        type="tel"
+        autocomplete="tel"
+    />
+
+    <span>
+        <!-- Placeholder for layout purposes -->
+    </span>
+
+    <flux:separator class="sm:col-span-2" />
+
     <flux:button
         type="submit"
         label="Absenden"
