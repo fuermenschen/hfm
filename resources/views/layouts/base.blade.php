23521319a7c8ef4349e1ffdd6efeb6b59858229f
Simon Moser <simon.github@mailbox.org>
2025-05-21 20:52:23 +0200

    Rename `icons` directory to `favicons` and update references.
    Moved all files from `public/icons` to `public/favicons` for better organization. Updated all corresponding references in the codebase to ensure seamless integration with the new path.

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -39,22 +39,22 @@
     <meta name="og:site_name" content="Höhenmeter für Menschen">
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ url(asset('icons/apple-icon-57x57.png')) }}">
-    <link rel="apple-touch-icon" sizes="60x60" href="{{ url(asset('icons/apple-icon-60x60.png')) }}">
-    <link rel="apple-touch-icon" sizes="72x72" href="{{ url(asset('icons/apple-icon-72x72.png')) }}">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ url(asset('icons/apple-icon-76x76.png')) }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ url(asset('icons/apple-icon-114x114.png')) }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ url(asset('icons/apple-icon-120x120.png')) }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ url(asset('icons/apple-icon-144x144.png')) }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ url(asset('icons/apple-icon-152x152.png')) }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ url(asset('icons/apple-icon-180x180.png')) }}">
-    <link rel="icon" type="image/png" sizes="192x192" href="{{ url(asset('icons/android-icon-192x192.png')) }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ url(asset('icons/favicon-32x32.png')) }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ url(asset('icons/favicon-96x96.png')) }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url(asset('icons/favicon-16x16.png')) }}">
-    <link rel="manifest" href="{{ url(asset('icons/manifest.json')) }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ url(asset('favicons/apple-icon-57x57.png')) }}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{ url(asset('favicons/apple-icon-60x60.png')) }}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{ url(asset('favicons/apple-icon-72x72.png')) }}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ url(asset('favicons/apple-icon-76x76.png')) }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ url(asset('favicons/apple-icon-114x114.png')) }}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{ url(asset('favicons/apple-icon-120x120.png')) }}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{ url(asset('favicons/apple-icon-144x144.png')) }}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{ url(asset('favicons/apple-icon-152x152.png')) }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url(asset('favicons/apple-icon-180x180.png')) }}">
+    <link rel="icon" type="image/png" sizes="192x192" href="{{ url(asset('favicons/android-icon-192x192.png')) }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url(asset('favicons/favicon-32x32.png')) }}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ url(asset('favicons/favicon-96x96.png')) }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url(asset('favicons/favicon-16x16.png')) }}">
+    <link rel="manifest" href="{{ url(asset('favicons/manifest.json')) }}">
     <meta name="msapplication-TileColor" content="#1B2E47">
-    <meta name="msapplication-TileImage" content="{{ url(asset('icons/ms-icon-144x144.png')) }}">
+    <meta name="msapplication-TileImage" content="{{ url(asset('favicons/ms-icon-144x144.png')) }}">
     <meta name="theme-color" content="#1B2E47">
 
     <!-- Fonts -->
