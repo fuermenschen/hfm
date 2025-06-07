51c67fd50db0d87336f7e077fce33b0958c61d0d
Simon Moser <simon.github@mailbox.org>
2025-06-07 16:01:09 +0200

    Update privacy policy and related components for improved clarity
    Revised privacy text in `BecomeDonatorForm.php` and `BecomeAthleteForm.php` to specify data use for event organization. Updated privacy page details to include data retention policies, hosting limitations, and added sections for club members. Enhanced transparency regarding data handling practices.

--- app/Components/BecomeDonatorForm.php
+++ app/Components/BecomeDonatorForm.php
@@ -225,7 +225,7 @@ class BecomeDonatorForm extends Component
     {
         $this->dialog([
             'title' => 'Datenschutz',
-            'description' => "Wir benutzen deine Daten nur für Zwecke, die für die Organisation zwingend sind. Nach dem Anlass werden deine Daten gelöscht. Es werden niemals Daten an Dritte weitergegeben. Mehr Informationen findest du in der Datenschutzerklärung.<br><br><a href='/datenschutz' target='_blank' class='underline'>Datenschutzerklärung</a>",
+            'description' => "Wir benutzen deine Daten nur für die Organisation des Anlasses Höhenmeter für Menschen. Es werden niemals Daten an Dritte weitergegeben. Mehr Informationen findest du in der Datenschutzerklärung.<br><br><a href='/datenschutz' target='_blank' class='underline'>Datenschutzerklärung</a>",
             'icon' => 'info',
         ]);
     }
--- resources/views/pages/privacy.blade.php
+++ resources/views/pages/privacy.blade.php
@@ -7,45 +7,47 @@
 
     <p>Der Schutz deiner Daten ist uns wichtig. Und das ist keine Floskel, sondern meinen wir ernst. Wir versuchen, nur
         Daten zu
-        erfassen, die wir für die Organisation des Anlasses benötigen. Das sind die folgenden:</p>
+        erfassen, die wir für die Organisation des Anlasses und unsere Vereinstätigkeit benötigen. Wir geben deine
+        Personendaten niemals weiter.</p>
 
     <x-page-subtitle>Allgemeine:r Websitenbenutzer:in</x-page-subtitle>
-    <p>Wenn du unsere Website besuchst, erfassen wir keine Daten von dir. Wir verwenden keine Cookies und erfassen keine
-        IP-Adressen. Wir verwenden auch keine Analysetools, um das Verhalten der Besucher:innen zu analysieren.</p>
+    <p>Wenn du unsere Website besuchst, erfassen wir keine Daten von dir. Wir verwenden keine Tracking-Cookies und
+        erfassen keine
+        IP-Adressen. Wir verwenden auch keine Analysetools, um das Verhalten der Besucher:innen zu analysieren.
+        Allerdings ist es technisch unmöglich, dass deine IP-Adresse nicht an unseren Webserver gesendet wird
+        (siehe
+        <x-inline-link href="https://de.wikipedia.org/wiki/Transmission_Control_Protocol" target="_blank">TCP
+        </x-inline-link>
+        ). Ob, wie und wie lange unser Hosting Anbieter
+        <x-inline-link href="https://www.infomaniak.com/de" target="_blank">Infomaniak</x-inline-link>
+        diese IP-Adresse speichert, können wir nicht beeinflussen.
+    </p>
 
     <x-page-subtitle>Sportler:innen</x-page-subtitle>
-    Wir erfassen von dir personenbezogene Daten, die du uns bei der Anmeldung zum Anlass gibst. Das sind:
-    <ul class="list-disc m-sm">
-        <li>Name</li>
-        <li>Vorname</li>
-        <li>Volljährigkeit</li>
-        <li>Adresse</li>
-        <li>E-Mail-Adresse</li>
-        <li>Telefonnummer</li>
-        <li>Volljährigkeit</li>
-    </ul>
+    <p>Wir erfassen von dir Personendaten, die du uns bei der Anmeldung zum Anlass gibst.</p>
 
     <p>Bei der Anmeldung der Spender:innen wird dein Name angezeigt, sodass die Spender:innen dich auswählen können (Nur
         Initiale des Nachnamens, z.B. Francesca A.). Die anderen Angaben brauchen wir, damit wir dir ein Starter-Paket
         senden können und, falls du noch nicht volljährig bist, deine Eltern über deine Anmeldung informieren können.
         Sobald der Anlass und dessen Nachbearbeitung abgeschlossen sind, werden alle personenbezogenen Daten
-        gelöscht.</p>
+        gelöscht. Ausgenommen hiervon ist dein Name und deine E-Mail-Adresse. Diese behalten wir, um dich über weitere
+        Anlässe oder Angebote von uns zu informieren.</p>
 
     <x-page-subtitle>Spender:innen</x-page-subtitle>
-    <p>Wir erfassen von dir personenbezogene Daten, die du uns bei der Anmeldung als Spender:in gibst. Das sind:</p>
-    <ul class="list-disc m-sm">
-        <li>Name</li>
-        <li>Vorname</li>
-        <li>Adresse</li>
-        <li>E-Mail-Adresse</li>
-        <li>Telefonnummer</li>
-    </ul>
+    <p>Wir erfassen von dir personenbezogene Daten, die du uns bei der Anmeldung als Spender:in gibst.</p>
 
     <p> Dein Name (Nur Vorname und Initiale des Nachnamens, z.B. Francesca A.) ist für die:den Sportler:in, welche du
         ausgewählt hast, sichtbar. Die anderen Angaben brauchen wir, damit wir dich erreichen können und dir nach dem
         Anlass eine Rechnung zustellen können.
         Sobald der Anlass und dessen Nachbearbeitung abgeschlossen sind, werden alle personenbezogenen Daten
-        gelöscht.</p>
+        gelöscht. Ausgenommen hiervon ist dein Name und deine E-Mail-Adresse. Diese behalten wir, um dich über weitere
+        Anlässe oder Angebote von uns zu informieren. Wenn du eine Spendenrechnung einzahlst, werden Personendaten von
+        dir an unsere Bank (ZKB) übermittelt une über mehrere Jahre gespeichert (gesetzliche Fristen). Auf diesem Weg
+        haben wir weiterhin Zugriff auf deine Informationen.</p>
+
+    <x-page-subtitle>Vereinsmitglieder</x-page-subtitle>
+    <p>Wir erfassen von dir personenbezogene Daten, die du uns bei der Anmeldung als Mitglied gibst.</p>
+    <p>Details zum Umgang mit Daten von Vereinsmitgliedern findest du in den Statuten.</p>
 
     <x-page-subtitle>Verantwortliche:r für die Datenverarbeitung</x-page-subtitle>
     <p>Verantwortlich für die Datenverarbeitung ist der <strong>Verein für Menschen</strong>. Bei Fragen
