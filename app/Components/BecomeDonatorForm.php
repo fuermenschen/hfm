b4351d3f07d23d8c256b314e7ad945f12eb01c13
Simon Moser <simon.github@mailbox.org>
2025-08-28 10:21:48 +0200

    Update BecomeDonatorForm validation and refactor form components
    - Replaced string validation with regex for phone number format in `BecomeDonatorForm`.
    - Migrated form components to use `flux:input`, improving consistency and reducing dependency on `x-input`.
    - Updated `flux:select`, `flux:textarea`, and other elements for enhanced readability and maintainability.

--- app/Components/BecomeDonatorForm.php
+++ app/Components/BecomeDonatorForm.php
@@ -52,7 +52,7 @@ class BecomeDonatorForm extends Component
     // Telefonnummer
     #[Validate('required', message: 'Wir benötigen deine Telefonnummer.')]
     #[Validate('string', message: 'Wir benötigen deine Telefonnummer.')]
-    #[Validate('size:10', message: 'Die Telefonnummer besteht aus 10 Zahlen.')]
+    #[Validate('regex:/^0\\d{2}\\s\\d{3}\\s\\d{2}\\s\\d{2}/', message: 'Die Telefonnummer ist ungültig.')]
     public ?string $phone_number = null;
 
     // E-Mail
