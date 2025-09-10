b8c67840343a92bb9b5f13c5cf20a93a13b37712
Simon Moser <simon.github@mailbox.org>
2025-09-10 06:16:55 +0200

    feat: remove donator invoice functionality and associated fields
    - Deleted donator invoice Blade template and printing logic.
    - Dropped related columns (`invoice_paid`, `invoice_paid_at`, `invoice_sent`) from `donators` table via migration.
    - Removed invoice-related buttons, toggles, and actions in `AdminDonatorTable` component.

--- app/Components/AdminDonatorTable.php
+++ app/Components/AdminDonatorTable.php
@@ -3,10 +3,8 @@
 namespace App\Components;
 
 use App\Models\Donator;
-use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Carbon;
-use Livewire\Attributes\On;
 use PowerComponents\LivewirePowerGrid\Button;
 use PowerComponents\LivewirePowerGrid\Column;
 use PowerComponents\LivewirePowerGrid\Components\SetUp\Exportable;
@@ -27,33 +25,7 @@ class AdminDonatorTable extends PowerGridComponent
 
     public function header(): array
     {
-        return [
-            Button::add('download')
-                ->slot('Download')
-                ->class('focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md ring-1 transition focus-within:ring-2 dark:ring-pg-primary-600 dark:text-pg-primary-300 text-gray-600 ring-gray-300 dark:bg-pg-primary-800 bg-white dark:placeholder-pg-primary-400 rounded-md border-0 bg-transparent py-2 px-3 ring-0 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 w-auto')
-                ->dispatch('batchDownload', [])
-                ->tooltip('Rechnung für ausgewählte Spender:innen herunterladen'),
-            Button::add('gesendet')
-                ->slot('gesendet')
-                ->class('focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md ring-1 transition focus-within:ring-2 dark:ring-pg-primary-600 dark:text-pg-primary-300 text-gray-600 ring-gray-300 dark:bg-pg-primary-800 bg-white dark:placeholder-pg-primary-400 rounded-md border-0 bg-transparent py-2 px-3 ring-0 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 w-auto')
-                ->dispatch('invoiceSent', ['sent' => true])
-                ->tooltip('Rechnungen als gesendet markieren'),
-            Button::add('nicht gesendet')
-                ->slot('nicht gesendet')
-                ->class('focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md ring-1 transition focus-within:ring-2 dark:ring-pg-primary-600 dark:text-pg-primary-300 text-gray-600 ring-gray-300 dark:bg-pg-primary-800 bg-white dark:placeholder-pg-primary-400 rounded-md border-0 bg-transparent py-2 px-3 ring-0 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 w-auto')
-                ->dispatch('invoiceSent', ['sent' => false])
-                ->tooltip('Rechnungen als nicht gesendet markieren'),
-            Button::add('bezahlt')
-                ->slot('bezahlt')
-                ->class('focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md ring-1 transition focus-within:ring-2 dark:ring-pg-primary-600 dark:text-pg-primary-300 text-gray-600 ring-gray-300 dark:bg-pg-primary-800 bg-white dark:placeholder-pg-primary-400 rounded-md border-0 bg-transparent py-2 px-3 ring-0 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 w-auto')
-                ->dispatch('invoicePaid', ['paid' => true])
-                ->tooltip('Rechnungen als bezahlt markieren'),
-            Button::add('nicht bezahlt')
-                ->slot('nicht bezahlt')
-                ->class('focus:ring-primary-600 focus-within:focus:ring-primary-600 focus-within:ring-primary-600 dark:focus-within:ring-primary-600 flex rounded-md ring-1 transition focus-within:ring-2 dark:ring-pg-primary-600 dark:text-pg-primary-300 text-gray-600 ring-gray-300 dark:bg-pg-primary-800 bg-white dark:placeholder-pg-primary-400 rounded-md border-0 bg-transparent py-2 px-3 ring-0 placeholder:text-gray-400 focus:outline-none sm:text-sm sm:leading-6 w-auto')
-                ->dispatch('invoicePaid', ['paid' => false])
-                ->tooltip('Rechnungen als nicht bezahlt markieren'),
-        ];
+        return [];
     }
 
     public function setUp(): array
@@ -113,8 +85,7 @@ class AdminDonatorTable extends PowerGridComponent
                 return 'Fr. '.number_format($this_sum, 2, '.', "'");
             })
             ->add('created_at_formatted', fn ($donator) => Carbon::parse($donator->created_at)->format('d.m.Y'))
-            ->add('invoice_sent_at_formatted', fn ($donator) => $donator->invoice_sent_at ? Carbon::parse($donator->invoice_sent_at)->format('d.m.Y') : null)
-            ->add('invoice_paid_at_formatted', fn ($donator) => $donator->invoice_paid_at ? Carbon::parse($donator->invoice_paid_at)->format('d.m.Y') : null)
+            ->add('invoice_sent_at_formatted', fn ($donator) => $donator->invoice_sent_at ? Carbon::parse($donator->invoice_sent_at)->format('d.m.Y H:i') : null)
             ->add('country_of_residence', fn ($donator) => $donator->country_of_residence);
     }
 
@@ -167,20 +138,7 @@ class AdminDonatorTable extends PowerGridComponent
                 ->sortable()
                 ->searchable(),
 
-            Column::make('Rechnung gesendet', 'invoice_sent')
-                ->sortable()
-                ->toggleable()
-                ->fixedOnResponsive(),
-
             Column::make('Rechnung gesendet am', 'invoice_sent_at_formatted', 'invoice_sent_at')
-                ->sortable(),
-
-            Column::make('Rechnung bezahlt', 'invoice_paid')
-                ->sortable()
-                ->toggleable()
-                ->fixedOnResponsive(),
-
-            Column::make('Rechnung bezahlt am', 'invoice_paid_at_formatted', 'invoice_paid_at')
                 ->sortable(),
 
             Column::action('Aktionen')
@@ -195,146 +153,9 @@ class AdminDonatorTable extends PowerGridComponent
         ];
     }
 
-    // Actions
-    public function onUpdatedToggleable(string|int $id, string $field, string $value): void
-    {
-        $worked = false;
-
-        try {
-
-            $donator = Donator::findOrFail($id);
-
-            switch ($field) {
-                case 'invoice_sent':
-                    $donator->invoice_sent = (bool) $value;
-                    if ($value) {
-                        $donator->invoice_sent_at = Carbon::now();
-                    } else {
-                        $donator->invoice_sent_at = null;
-                    }
-                    $donator->save();
-                    $worked = true;
-                    break;
-
-                case 'invoice_paid':
-                    $donator->invoice_paid = (bool) $value;
-                    if ($value) {
-                        $donator->invoice_paid_at = Carbon::now();
-                    } else {
-                        $donator->invoice_paid_at = null;
-                    }
-                    $donator->save();
-                    $worked = true;
-                    break;
-            }
-
-            $this->skipRender();
-        } catch (\Exception $e) {
-            $worked = false;
-        }
-
-        if ($worked) {
-            $this->notification()->success('Erfolgreich gespeichert.');
-        } else {
-            $this->notification()->error('Fehler beim Speichern.');
-        }
-    }
-
-    #[On('invoiceSent')]
-    public function invoiceSent(bool $sent)
-    {
-        if ($this->checkboxValues) {
-            foreach ($this->checkboxValues as $id) {
-                $donator = Donator::findOrfail($id);
-                $donator->invoice_sent = $sent;
-                if ($sent) {
-                    $donator->invoice_sent_at = Carbon::now();
-                } else {
-                    $donator->invoice_sent_at = null;
-                }
-                $donator->save();
-            }
-            $this->notification()->success('Rechnungen als gesendet markiert (Seite muss aktualisiert werden)');
-        } else {
-            $this->notification()->error('Keine Spender:innen ausgewählt.');
-        }
-
-        $this->checkboxValues = [];
-
-    }
-
-    #[On('invoicePaid')]
-    public function invoicePaid(bool $paid)
-    {
-        if ($this->checkboxValues) {
-            foreach ($this->checkboxValues as $id) {
-                $donator = Donator::findOrfail($id);
-                $donator->invoice_paid = $paid;
-                if ($paid) {
-                    $donator->invoice_paid_at = Carbon::now();
-                } else {
-                    $donator->invoice_paid_at = null;
-                }
-                $donator->save();
-            }
-            $this->notification()->success('Rechnungen als bezahlt markiert (Seite muss aktualisiert werden)');
-        } else {
-            $this->notification()->error('Keine Spender:innen ausgewählt.');
-        }
-
-        $this->checkboxValues = [];
-
-    }
-
-    #[On('batchDownload')]
-    public function batchDownload()
-    {
-        // create a zip file with all invoices
-        $zip = new \ZipArchive;
-
-        $filename = 'Rechnungen_'.Carbon::now()->format('Y-m-d').'.zip';
-
-        if ($zip->open(storage_path('app/'.$filename), \ZipArchive::CREATE) === true) {
-            if ($this->checkboxValues) {
-                foreach ($this->checkboxValues as $id) {
-                    $this_invoice = $this->downloadInvoice($id, false);
-                    $zip->addFromString($this_invoice['filename'], $this_invoice['pdf']->output());
-                }
-            }
-            $zip->close();
-        }
-
-        return response()->download(storage_path('app/'.$filename))->deleteFileAfterSend(true);
-    }
-
-    #[On('downloadInvoice')]
-    public function downloadInvoice($donator_id, $download = true)
-    {
-        $donator = Donator::findOrfail($donator_id);
-        $donations = $donator->donations()->with(['athlete', 'athlete.partner'])->get();
-        $filename = sprintf('DON-24%04d_', $donator_id).$donator->first_name.'_'.$donator->last_name.'_Rechnung.pdf';
-        $pdf = Pdf::loadView('printables.donator_invoice', ['donator' => $donator, 'donations' => $donations])
-            ->setPaper('a4', 'portrait');
-
-        // dd($pdf);
-
-        if ($download) {
-            return response()->streamDownload(function () use ($pdf) {
-                echo $pdf->stream();
-            }, $filename);
-        } else {
-            return ['pdf' => $pdf, 'filename' => $filename];
-        }
-    }
-
     public function actions(Donator $row): array
     {
         return [
-            Button::add('Rechnung')
-                ->slot('PDF Rechnung')
-                ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
-                ->dispatch('downloadInvoice', ['donator_id' => $row->id])
-                ->tooltip('Rechnung herunterladen'),
             Button::add('loginAsDonator')
                 ->slot('Login')
                 ->class('pg-btn-white dark:ring-pg-primary-600 dark:border-pg-primary-600 dark:hover:bg-pg-primary-700 dark:ring-offset-pg-primary-800 dark:text-pg-primary-300 dark:bg-pg-primary-700')
---
+++ database/migrations/2025_09_10_060500_remove_invoice_paid_fields_from_donators.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Remove invoice_paid related columns from donators
+        Schema::table('donators', function (Blueprint $table) {
+            if (Schema::hasColumn('donators', 'invoice_paid_at')) {
+                $table->dropColumn('invoice_paid_at');
+            }
+            if (Schema::hasColumn('donators', 'invoice_paid')) {
+                $table->dropColumn('invoice_paid');
+            }
+            if (Schema::hasColumn('donators', 'invoice_sent')) {
+                $table->dropColumn('invoice_sent');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('donators', function (Blueprint $table) {
+            if (! Schema::hasColumn('donators', 'invoice_paid')) {
+                $table->boolean('invoice_paid')->default(false);
+            }
+            if (! Schema::hasColumn('donators', 'invoice_paid_at')) {
+                $table->timestamp('invoice_paid_at')->nullable();
+            }
+            if (! Schema::hasColumn('donators', 'invoice_sent')) {
+                $table->boolean('invoice_sent')->default(false);
+            }
+        });
+    }
+};
