61c4afafd061aec472efcc723ad9978d45645403
KaiFrehner <kaifrehner@gmail.com>
2026-09-19 11:00:17 +0200

    standardize impressum address with billing address.

--- resources/views/pages/impressum.blade.php
+++ resources/views/pages/impressum.blade.php
@@ -5,9 +5,23 @@
         Impressum
     @endcomponent
 
+    @php
+        $invoiceSettings = app(\App\Settings\InvoiceSettings::class);
+
+        $addressLines = array_values(array_filter([
+            trim((string) $invoiceSettings->creditor_name),
+            (string) $invoiceSettings->creditor_care_of !== '' ? 'c/o '.trim((string) $invoiceSettings->creditor_care_of) : null,
+            trim((string) $invoiceSettings->creditor_street.' '.(string) $invoiceSettings->creditor_building_number),
+            trim((string) $invoiceSettings->creditor_postal_code.' '.(string) $invoiceSettings->creditor_city),
+        ], fn ($line): bool => $line !== null && $line !== ''));
+
+        if ($addressLines === []) {
+            $addressLines = ['Verein für Menschen', 'c/o Kai Frehner', 'Rössligasse 6', '8405 Winterthur'];
+        }
+    @endphp
+
     <p>Diese Website ist ein Projekt von:</p>
-    <p>Verein für Menschen</p>
-    <p>c/o Kai Frehner</p>
-    <p>Nelkenstrasse 6</p>
-    <p>8400 Winterthur</p>
+    @foreach ($addressLines as $line)
+        <p>{{ $line }}</p>
+    @endforeach
 @endsection
